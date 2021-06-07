0de138f9748a799763de203cd5938bde188a14ce
akshita2903 <68686051+akshita2903@users.noreply.github.com>
2021-06-07 19:06:10 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿var html,css,js;
+function run(){
+ html=document.getElementById("htmlcode").value;
+ css="<style>" + document.getElementById("csscode").value + "</style>";
+ js="<scri" + "pt>" + document.getElementById("jscode").value+ "</scri" + "pt>";
+var result=document.getElementById("preview-window").contentWindow.document;
+result.open();
+result.write(html+css+js);
+result.close();
+}
